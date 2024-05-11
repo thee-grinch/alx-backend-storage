d05fcb8eb36662427d415bd54e07450848d45a95
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2024-05-11 23:58:53 +0300

    task 4

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -2,11 +2,12 @@
 
 delimiter //
 CREATE TRIGGER decrease_quantity
-AFTER INSERT ON orders
+AFTER
+INSERT ON orders
 FOR EACH ROW
 BEGIN
     UPDATE items
     SET quantity = quantity - 1
-    WHERE id = NEW.item_id;
+    WHERE name = NEW.item_name;
 END//
 delimiter ;
