8514de8f80258b8eee6767a8887d0183f1829599
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2024-05-13 20:17:49 +0300

    task 6

---
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -0,0 +1,21 @@
+-- a  SQL script that creates a stored procedure AddBonus that adds a new correction for a student
+
+DELIMITER //
+CREATE PROCEDURE AddBonus(IN user_id INT, IN  project_name VARCHAR(255), IN score INT)
+BEGIN
+    DECLARE project_id INT;
+    SELECT id INTO project_id FROM projects WHERE projects.name = project_name;
+    if (project_id IS NULL) THEN
+    BEGIN
+        INSERT INTO projects (name)
+        VALUES (project_name);
+        SET project_id = LAST_INSERT_ID()
+    END;
+    END IF;
+    INSERT INTO corrections (user_id, project_id, score)
+    VALUES (user_id, project_id, score);
+
+    COMMIT;
+END //
+
+DELIMITER ;
