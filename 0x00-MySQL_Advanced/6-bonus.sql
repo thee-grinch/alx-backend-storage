ade527c3b3e01ff75afafa47a49a3a6fb728b7b2
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2024-05-14 14:47:50 +0300

    task 6

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -8,13 +8,11 @@ BEGIN
     if (project_id IS NULL) THEN
         BEGIN
          INSERT INTO projects (name) VALUES (project_name);
-         SET project_id = LAST_INSERT_ID();~
+         SET project_id = LAST_INSERT_ID();
         END;
     END IF;
     INSERT INTO corrections (user_id, project_id, score)
     VALUES (user_id, project_id, score);
-
-    COMMIT;
 END //
 
 DELIMITER ;
